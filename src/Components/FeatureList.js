3da4f7c6470f70c068a6a4f939768b8d8a26b499
Chris McArdle <christopher.mcardle2@gmail.com>
2020-05-18 18:08:13 -0500

    added files

---
+++ src/Components/FeatureList.js
@@ -0,0 +1,48 @@
+
+import React, { Component } from 'react';
+import SelectPart from './SelectPart.js';
+import slugify from 'slugify';
+
+
+
+export default class FeatureList extends Component{
+  render(){
+
+        const features = Object.keys(this.props.features).map((feature,idx) => {
+      const featureHash = feature + '-' + idx;
+      const options = this.props.features[feature].map(option => {
+      const itemHash = slugify(JSON.stringify(option));
+    return (
+      <SelectPart
+
+      itemHash={itemHash} 
+      feature={feature} 
+      option={option} 
+      {...this.props} 
+    
+      />
+    );
+
+    });
+
+    return(
+
+      <fieldset className="feature" key={featureHash}>
+      <legend className="feature__name">
+        <h3>{feature}</h3>
+      </legend>
+      {options}
+    </fieldset>
+    );
+      });
+
+    return (
+        <form className="main__form">
+          <h3>Customize Your Laptop</h3>
+          {features}
+        </form>
+      );  
+    
+  }
+}
+
---
+++ src/Components/Heading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Heading extends Component {
+  render(){
+    return(
+      <header>
+        <h1>ELF Computers | Laptops</h1>
+      </header>
+    );
+  }
+}
+
